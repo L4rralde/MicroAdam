cea9a04a6b974398b71c31c67b394d4fc58bd76f
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-06-10 11:40:06 -0600

    Moving training function to module

--- scripts/breast_cancer.py
+++ scripts/breast_cancer.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from utils.utils import DEVICE
 from micro_adam.micro_adam import MicroAdam
 from dataset.breast_cancer import train_dataset, test_dataset
+from train.train import train
 
 
 class Mlp(nn.Module):
@@ -19,58 +20,6 @@ class Mlp(nn.Module):
     def forward(self, x):
         x = x.float()
         return self.layer(x)
-
-
-def train(
-    model: nn.Module,
-    train_dataloader: object,
-    val_dataloader: object,
-    loss_fn: object,
-    optimizer: object,
-    epochs=100,
-    verbose=True
-):
-    train_losses = []
-    val_losses = []
-    for epoch in range(1, epochs + 1):
-        train_loss = 0.0
-        num_train_elements = 0
-        model.train()
-        for x, y in train_dataloader:
-            x = x.to(DEVICE)
-            y = y.to(DEVICE)
-            pred = model(x)
-            loss = loss_fn(pred, y)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            batch_size = x.size(0)
-            train_loss += loss.data.item() * batch_size
-            num_train_elements += batch_size
-        
-        train_loss /= num_train_elements
-        train_losses.append(train_loss)
-
-        val_loss = 0.0
-        num_val_elements = 0
-        model.eval()
-        with torch.no_grad():
-            for x, y in val_dataloader:
-                x = x.to(DEVICE)
-                y = y.to(DEVICE)
-                pred = model(x)
-                loss = loss_fn(pred, y)
-
-                batch_size = x.size(0)
-                val_loss += loss.data.item() * batch_size
-                num_val_elements += batch_size
-        val_loss /= num_val_elements
-        val_losses.append(val_loss)
-        if verbose:
-            print(f"Epoch: {epoch}. Training loss: {train_loss: .3e}. Validation loss: {val_loss: .3e}")
-
-    return train_losses, val_losses
 
 
 def main():
